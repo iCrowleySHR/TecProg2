593dfcc91ab21bc2b310f21049291f7f0e6437dd
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2025-10-02 09:16:05 -0300

    feat: aula 1

---
+++ Aula01-TecProg2/Aula01-TecProg2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Aula01_TecProg2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputIdade = new System.Windows.Forms.TextBox();
+            this.labelIdade = new System.Windows.Forms.Label();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputIdade
+            // 
+            this.inputIdade.Location = new System.Drawing.Point(76, 77);
+            this.inputIdade.Name = "inputIdade";
+            this.inputIdade.Size = new System.Drawing.Size(320, 22);
+            this.inputIdade.TabIndex = 0;
+            // 
+            // labelIdade
+            // 
+            this.labelIdade.AutoSize = true;
+            this.labelIdade.Location = new System.Drawing.Point(73, 58);
+            this.labelIdade.Name = "labelIdade";
+            this.labelIdade.Size = new System.Drawing.Size(42, 16);
+            this.labelIdade.TabIndex = 1;
+            this.labelIdade.Text = "Idade";
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnExibir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExibir.Location = new System.Drawing.Point(76, 132);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(82, 30);
+            this.btnExibir.TabIndex = 2;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = false;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnApagar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApagar.Location = new System.Drawing.Point(173, 132);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(86, 30);
+            this.btnApagar.TabIndex = 3;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = false;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.labelIdade);
+            this.Controls.Add(this.inputIdade);
+            this.Name = "Form1";
+            this.Text = "Form1";
+    
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputIdade;
+        private System.Windows.Forms.Label labelIdade;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Button btnApagar;
+    }
+}
+
---
+++ Aula01-TecProg2/Aula01-TecProg2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula01_TecProg2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            inputIdade.Clear();
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            int idade;
+
+            if (int.TryParse(inputIdade.Text, out idade))
+            {
+                if (idade >= 18)
+                {
+                    inputIdade.Clear();
+                    MessageBox.Show("Maior de idade");
+                }
+                else
+                {
+                    inputIdade.Clear();
+                    MessageBox.Show("Menor de idade");
+                }
+            }
+            else
+            {
+                inputIdade.Clear();
+                MessageBox.Show("Valor diferente de inteiro no campo idade");
+            }
+        }
+
+    }
+}
